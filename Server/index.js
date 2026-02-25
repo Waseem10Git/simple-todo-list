b5d8df17a050cfbd3efed7ab613d6d40cad8b922
Waseem10Git <wasim10ghabour@gmail.com>
2026-02-25 15:14:04 +0300

    General improvements

--- Server/index.js
+++ Server/index.js
@@ -7,36 +7,39 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/simpletodolist")
+mongoose.connect("mongodb://localhost:27017/");
 
-app.get('/', (req, res) => {
-    TodoModel.find()
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+app.get("/", (req, res) => {
+  TodoModel.find()
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
 
-app.post('/add', (req, res) => {
-    const task = req.body.task;
-    TodoModel.create({
-        task: task
-    }).then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+app.post("/add", (req, res) => {
+  const task = req.body.task;
+  TodoModel.create({
+    task: task,
+  })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
 
-app.put('/update/:id', (req, res) => {
-    const id = req.params.id;
-    TodoModel.findByIdAndUpdate(id, {done: true})
-    .then(result => res.json(result))    
-    .catch(err => res.json(err))    
-})
+app.put("/update/:id", (req, res) => {
+  const id = req.params.id;
+  TodoModel.findByIdAndUpdate(id, [{ $set: { done: { $not: "$done" } } }], {
+    new: true,
+  })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
 
-app.delete('/delete/:id', (req, res) => {
-    const id = req.params.id;
-    TodoModel.findByIdAndDelete(id)
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  TodoModel.findByIdAndDelete(id)
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
 
 app.listen(4001, () => {
-    console.log("Server is running on port 4001");
-})
+  console.log("Server is running on port 4001");
+});
--- todolist/src/Create.jsx
+++ todolist/src/Create.jsx
@@ -1,32 +1,41 @@
-import { useState, useEffect, useRef } from "react"
-import axios from "axios"
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
-function Create ({onTaskAdded}) {
-    const [task, setTask] = useState("");
-    const inputRef = useRef(null);
+function Create({ onTaskAdded }) {
+  const [task, setTask] = useState("");
+  const inputRef = useRef(null);
 
-    useEffect(() => {
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleAdd = () => {
+    if (task.trim() === "") return;
+
+    axios
+      .post("http://localhost:4001/add", { task: task })
+      .then(() => {
+        setTask("");
         inputRef.current.focus();
-    }, []);
+        onTaskAdded();
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const handleAdd = () => {
-        if (task.trim() === "") return;
-
-        axios.post('http://localhost:4001/add', {task: task})
-        .then(result => {
-            setTask("");
-            inputRef.current.focus();
-            onTaskAdded();
-        })
-        .catch(err => console.log(err))
-    }
-
-    return (
-        <div className="create_form">
-            <input type="text" ref={inputRef} placeholder="Enter Task" value={task} onChange={(e) => setTask(e.target.value)} />
-            <button type="button" onClick={handleAdd}>Add</button>
-        </div>
-    )
+  return (
+    <div className="create_form">
+      <input
+        type="text"
+        ref={inputRef}
+        placeholder="Enter Task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Add
+      </button>
+    </div>
+  );
 }
 
-export default Create
+export default Create;
